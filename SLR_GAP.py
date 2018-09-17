c049144fdc9ad741c2ffd0cb4d480a126a526d60
Mehrdad Shahabi <mehrdad.shahabi@gmail.com>
2018-09-17 14:13:23 -0400

    GAP code Created

---
+++ SLR_GAP.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 13:47:01 2018
+
+@author: mshahabi
+"""
+
+"""
+Solving a generalized assgnement problem by surrogate lagrangian relaxation
+
+"""
+
+import os
+import random
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+from pyomo.opt import SolverStatus, TerminationCondition
+from scipy import sparse
+import math
+import logging
+
+class Central_GAP:
+    
+    def __init__(self):
+        self.Create_Model()
+   
+    def Create_Model(self):
+        self.model = ConcreteModel()
+        self.model.nodes   = Set(initialize=range(0,10))
+        self.model.cost    = Param(self.model.nodes*self.model.nodes, initialize=random.randint(0,10))
+        self.model.b       = Param(self.model.nodes, initialize=random.randint(0,10))
+        self.model.x       = Var(self.model.nodes*self.model.nodes, within=Binary)
+        self.model.pprint()    
+        def obj_rule(model):
+            first_term  = sum(self.model.x[n]*self.model.cost[n] for n in self.model.nodes*self.model.nodes)          
+            return first_term
+            
+        self.model.obj     = Objective(rule=obj_rule,sense=minimize)
+            
+        def flow_balance_1(model,p):
+            return sum(self.model.x[n,p] for n in self.model.nodes)<=self.model.b[p]
+        
+        def flow_balance_2(model,n):          
+            return sum(self.model.x[n,p] for p in self.model.nodes)==1 
+        
+                
+        self.model.flowbal_1 = Constraint(self.model.nodes, rule=flow_balance_1)
+        self.model.flowbal_2 = Constraint(self.model.nodes, rule=flow_balance_1)
+            
+        
+    def solve(self):
+        instance = self.model
+        instance.display()
+        opt = SolverFactory("IPOPT")
+        results = opt.solve(instance)
+        test_flow = 0
+            
+        return results
+    
+a=Central_GAP()
+b=a.solve()

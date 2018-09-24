efb64692d542d7deebc4dfd68d219e67069780e4
Mehrdad Shahabi <mehrdad.shahabi@gmail.com>
2018-09-24 12:28:18 -0400

    Latest_Updates

--- SLR_GAP.py
+++ SLR_GAP.py
@@ -18,40 +18,53 @@ from pyomo.opt import SolverStatus, TerminationCondition
 from scipy import sparse
 import math
 import logging
+import numpy as np
+import cplex
 
 class Central_GAP:
     
-    def __init__(self):
+    def __init__(self,nbM,nbJ):
+        self.nbM = nbM
+        self.nbJ = nbJ
+        self.cost, self.b = self.gen_rand_problem()
         self.Create_Model()
-   
+    
+    def gen_rand_problem(self):
+        cost_ = np.empty([self.nbM,self.nbJ], dtype=float)
+        cap = np.empty([self.nbM], dtype=float)
+        for M in range(0, self.nbM):
+            cap[M] = random.randint(100,300)
+            for J in range(0, self.nbJ):
+                cost_[M,J] = random.randint(0,100)       
+        return cost_, cap
+        
     def Create_Model(self):
         self.model = ConcreteModel()
-        self.model.nodes   = Set(initialize=range(0,100))
-        self.model.cost    = Param(self.model.nodes*self.model.nodes, initialize=random.randint(0,100))
-        self.model.b       = Param(self.model.nodes, initialize=random.randint(80,150))
-        self.model.x       = Var(self.model.nodes*self.model.nodes, within=Binary)   
+        self.model.nbM   = Set(initialize=range(0,self.nbM))
+        self.model.nbJ   = Set(initialize=range(0,self.nbJ))
+        self.model.cost    = self.cost
+        self.model.b       = self.b 
+        self.model.x       = Var(self.model.nbM*self.model.nbJ, within=Binary)   
+
         def obj_rule(model):
-            first_term  = sum(self.model.x[n]*self.model.cost[n] for n in self.model.nodes*self.model.nodes)          
+            first_term  = sum(self.model.x[n]*self.model.cost[n] for n in self.model.nbM*self.model.nbJ)          
             return first_term
             
         self.model.obj     = Objective(rule=obj_rule,sense=minimize)
             
-        def flow_balance_1(model,p):
-            return sum(self.model.x[n,p] for n in self.model.nodes)<= self.model.b[p] 
+        def flow_balance_1(model,m):
+            return sum(self.model.x[m,j] for j in self.model.nbJ)<= self.model.b[m] 
         
-        def flow_balance_2(model,n):          
-            return sum(self.model.x[n,p] for p in self.model.nodes)==1 
-        
-                
-        self.model.flowbal_1 = Constraint(self.model.nodes, rule=flow_balance_1)
-        self.model.flowbal_2 = Constraint(self.model.nodes, rule=flow_balance_2)
+        def flow_balance_2(model,j):          
+            return sum(self.model.x[m,j] for m in self.model.nbM)==1 
+                       
+        self.model.flowbal_1 = Constraint(self.model.nbM, rule=flow_balance_1)
+        self.model.flowbal_2 = Constraint(self.model.nbJ, rule=flow_balance_2)
             
-        
     def solve(self, display_solution_stream=False , solve_relaxation = False):
         instance = self.model
         instance.preprocess()
-
-        opt = SolverFactory("cplex" )
+        opt = SolverFactory("Cplex" )
         opt.relax=1
         results = opt.solve(instance, tee=display_solution_stream)
         if (results.solver.status != pyomo.opt.SolverStatus.ok):
@@ -59,8 +72,8 @@ class Central_GAP:
         if (results.solver.termination_condition != pyomo.opt.TerminationCondition.optimal):  
             logging.warning('Check solver optimality?')
         instance.solutions.store_to(results)
-        for p in instance.nodes:
-            for q in instance.nodes:
+        for p in instance.nbM:
+            for q in instance.nbJ:
                 if instance.x[p,q].value==1:
                     print(p,q,instance.x[p,q].value)
         return instance 
@@ -100,7 +113,7 @@ class SLR_GAP:
         instance = self.model
         instance.preprocess()
         instance.display()
-        opt = SolverFactory("cplex" )
+        opt = SolverFactory("GLPK" )
         opt.relax = True
         results = opt.solve(instance, tee=display_solution_stream)
         if (results.solver.status != pyomo.opt.SolverStatus.ok):
@@ -117,6 +130,6 @@ class SLR_GAP:
     
 lambdaa = [-10 for i in range(0,10)] 
 node    = [i for i in range(0,10)]
-a=Central_GAP()
+a=Central_GAP(5,6)
 #a=SLR_GAP(lambdaa, node)
 b=a.solve(True, True)

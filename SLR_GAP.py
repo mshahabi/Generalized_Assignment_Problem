ebee0b3e5c574df649a72c1187b50d5275e453e8
Mehrdad Shahabi <mehrdad.shahabi@gmail.com>
2018-09-25 11:40:07 -0400

    Lagrangian Updated

--- SLR_GAP.py
+++ SLR_GAP.py
@@ -87,8 +87,8 @@ class SLR_SubProblem():
         self.model.x       = Var(self.model.nbM*self.model.nbJ, within=Binary)
   
         def obj_rule(model):
-            first_term  = sum(self.model.x[n]*(self.model.cost[n]+self.lambdaa[n[0]]) for n in self.model.nbM*self.model.nbJ)
-            second_term = sum(self.lambdaa[n[0]] for n in self.model.nbM*self.model.nbJ) 
+            first_term  = sum(self.model.x[n]*(self.model.cost[n]+self.lambdaa[n[1]]) for n in self.model.nbM*self.model.nbJ)
+            second_term = sum(self.lambdaa[j] for j in self.model.nbJ) 
             return first_term - second_term
             
         self.model.obj     = Objective(rule=obj_rule,sense=minimize)
@@ -98,10 +98,9 @@ class SLR_SubProblem():
                        
         self.model.flowbal_1 = Constraint(self.model.nbM, rule=flow_balance_1)            
         
-    def solve(self, display_solution_stream=False , solve_relaxation = True):
+    def solve_sp(self, display_solution_stream=False , solve_relaxation = False):
         instance = self.model
         instance.preprocess()
-        instance.display()
         opt = SolverFactory("cplex" )
         opt.relax = solve_relaxation
         results = opt.solve(instance, tee=display_solution_stream)
@@ -114,14 +113,14 @@ class SLR_SubProblem():
         for p in instance.nbM:
             for q in instance.nbJ:
                     x_s[p,q] = instance.x[p,q].value
-        return x_s 
+        return value(instance.obj), x_s 
     
 #########################Function to generate a random problem#################
 def gen_rand_problem(nbM,nbJ):
      cost_ = np.empty([nbM,nbJ], dtype=float)
      cap = np.empty([nbM], dtype=float)
      for M in range(0, nbM):
-         cap[M] = random.randint(0,8)
+         cap[M] = 2
          for J in range(0, nbJ):
              cost_[M,J] = random.randint(0,100)       
      return cost_, cap    
@@ -129,30 +128,36 @@ def gen_rand_problem(nbM,nbJ):
 solver_name =  "cplex"
 num_of_machines = 10
 num_of_jobs = 12
-relaxed_nbM = int(num_of_machines/2)
+relaxed_nbM = num_of_machines
 cost,cap = gen_rand_problem(num_of_machines,num_of_jobs)
+relaxed_cap = cap*3
  #Initializing Lambda0
 lambda_acum = {"x": [], "y": []}
-lambdaa = [-100 for i in range(0,num_of_jobs)] 
+lambdaa = [-10 for i in range(0,num_of_jobs)] 
 lambda_acum["x"].append(lambdaa[0])
 lambda_acum["y"].append(lambdaa[1])
 #SLR initial paramters
 alpha = 0.6
 M = 100
 r = 0.5
-ItrNum = 30
+ItrNum = 10
 #################2#EXACT RESULTS###############################################
 gen_exact_problem = Central_GAP(num_of_machines, num_of_jobs, cost, cap)
-display_solver_log = True
+display_solver_log = False
 relax_solution = False
 q_e, x_e = gen_exact_problem.solve(display_solver_log,relax_solution)  
 ####################SOLVING A RELAXED PROBLEM TO GET q_0#######################
-gen_exact_problem = Central_GAP(relaxed_nbM, num_of_jobs, cost, cap)
-display_solver_log = True
+gen_relax_problem = Central_GAP(relaxed_nbM, num_of_jobs, cost, relaxed_cap)
+display_solver_log = False
 relax_solution = False
-q_0, x_0 = gen_exact_problem.solve(display_solver_log,relax_solution)  
-# Evaluating the x_0 solution in lagrangian function
-Lagrang = sum(x_0[m,j]*(cost[m,j]+lambdaa[j]) for m in range(0,relaxed_nbM) for j in range(0,num_of_jobs)) - sum(lambdaa[j] for j in range(0,num_of_jobs))
+q_0, x_0 = gen_relax_problem.solve(display_solver_log,relax_solution)
+####################SOLVING A RELAXED PROBLEM TO GET Lagrangian function#######################  
+#Solving the relaxed problem to get Lagrangian function
+gen_lagrangian = SLR_SubProblem(lambdaa, num_of_machines, num_of_jobs, cost, cap)
+display_solver_log = False
+relax_solution = False
+Lagrang, x_0 = gen_lagrangian.solve_sp(solver_name, False)
+
 g_m = np.empty([num_of_jobs], dtype=int)
 for j in range(0,num_of_jobs):
     g_m[j] =  x_0[:,j].sum()-1
@@ -163,18 +168,20 @@ c_k_old = (q_0-Lagrang)/g_m_old
 lambdaa = lambdaa + c_k_old*g_m
 sub_sol = np.empty([num_of_machines, num_of_jobs], dtype=int) 
 for k in range(1, ItrNum):
+    print(lambdaa, c_k_old*g_m )
     sp = SLR_SubProblem(lambdaa, num_of_machines, num_of_jobs, cost, cap)
-    x_sp = sp.solve(solver_name, False)
+    _, x_sp = sp.solve_sp(solver_name, False)
 
     for j in range(0,num_of_jobs):
-        g_m[j] =  x_sp.sum()-1
+        g_m[j] =  x_sp[:,j].sum()-1
     g_m_new = sum(g_m**2)    
     if g_m_new ==0:
         g_m_new = 0.001
     p = 1 - 1/(k**r)
     alpha = 1- 1/(M*k**p)
     c_k = alpha*c_k_old*g_m_old/g_m_new
-    lambdaa = lambdaa + c_k*g_m_new
+    lambdaa = lambdaa + c_k*g_m
+
     g_m_old = g_m_new
     c_k_old = c_k
     obj = sum(x_sp[m,j]*cost[m,j] for m in range(0,num_of_machines) for j in range(0,num_of_jobs))

cf469ca22518a26bfa8971018ac3536f887951e5
Mehrdad Shahabi <mehrdad.shahabi@gmail.com>
2018-10-02 12:26:43 -0400

    Unbounded Solution Detected

--- SLAVR_GAP.py
+++ SLAVR_GAP.py
@@ -98,10 +98,14 @@ class SLR_SubProblem():
             return sum(self.model.x[m,j] for j in self.model.nbJ)<= self.model.b[m] 
         
         def flow_balance_2(model,j):          
-            return  -self.model.q[j] <= sum(self.model.x[m,j] for m in self.model.nbM)-1 <= self.model.q[j]
+            return  sum(self.model.x[m,j] for m in self.model.nbM)-1 <= self.model.q[j]
+        
+        def flow_balance_3(model,j):          
+            return  -self.model.q[j] <= sum(self.model.x[m,j] for m in self.model.nbM)-1
                        
         self.model.flowbal_1 = Constraint(self.model.nbM, rule=flow_balance_1)            
-        self.model.flowbal_2 = Constraint(self.model.nbJ, rule=flow_balance_2)    
+        self.model.flowbal_2 = Constraint(self.model.nbJ, rule=flow_balance_2)  
+        self.model.flowbal_3 = Constraint(self.model.nbJ, rule=flow_balance_3)  
         
     def solve_sp(self, display_solution_stream=False , solve_relaxation = False):
         instance = self.model
@@ -158,8 +162,8 @@ relax_solution = False
 q_0, x_0 = gen_relax_problem.solve(display_solver_log,relax_solution)
 ####################SOLVING A RELAXED PROBLEM TO GET Lagrangian function#######################  
 #Solving the relaxed problem to get Lagrangian function
-c_k_old = 1
-gen_lagrangian = SLR_SubProblem(c_k_old,lambdaa, num_of_machines, num_of_jobs, cost, cap)
+c_k_old = 0
+gen_lagrangian = SLR_SubProblem(lambdaa,c_k_old, num_of_machines, num_of_jobs, cost, cap)
 display_solver_log = False
 relax_solution = False
 Lagrang, x_0 = gen_lagrangian.solve_sp(solver_name, False)
@@ -175,7 +179,7 @@ lambdaa = lambdaa + c_k_old*g_m
 sub_sol = np.empty([num_of_machines, num_of_jobs], dtype=int) 
 for k in range(1, ItrNum):
     print(lambdaa, c_k_old*g_m )
-    sp = SLR_SubProblem(c_k_old,lambdaa, num_of_machines, num_of_jobs, cost, cap)
+    sp = SLR_SubProblem(lambdaa,c_k_old, num_of_machines, num_of_jobs, cost, cap)
     _, x_sp = sp.solve_sp(solver_name, False)
 
     for j in range(0,num_of_jobs):
